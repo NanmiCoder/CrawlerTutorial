7fc63d12d9c54c99552f65ef19d535db0513a638
程序员阿江(Relakkes) <relakkes@gmail.com>
2026-01-15 21:05:41 +0800

    fix: 修复03_代理IP的使用与管理目录下proxy_pool包的相对导入问题

--- 源代码/爬虫进阶/03_代理IP的使用与管理/proxy_pool/__init__.py
+++ 源代码/爬虫进阶/03_代理IP的使用与管理/proxy_pool/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # @Desc: 代理池模块
 
-from base import ProxyInfo, ProxyProtocol, IProxyFetcher, IProxyChecker, IProxyPool
-from checker import ProxyChecker
-from pool import ProxyPool
+from .base import ProxyInfo, ProxyProtocol, IProxyFetcher, IProxyChecker, IProxyPool
+from .checker import ProxyChecker
+from .pool import ProxyPool
 
 __all__ = [
     "ProxyInfo",
